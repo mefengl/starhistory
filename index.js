4f523b52e3947b3d42737bd9e0cf6520b21de9fc
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-11-13 01:13:41 +0800

    Add ESLint configuration and update package.json and index.js

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 #!/usr/bin/env node
 
-import getUrls from 'get-urls';
-import readline from 'readline';
-import open from 'open';
+import process from 'node:process'
+import readline from 'node:readline'
+import getUrls from 'get-urls'
+import open from 'open'
 
 function extractGitHubUrls(text) {
-  const urls = getUrls(text);
-  return Array.from(urls).filter(url => url.includes('github.com'));
+  const urls = getUrls(text)
+  return Array.from(urls).filter(url => url.includes('github.com'))
 }
 
 function generateStarHistoryLink(githubUrls) {
-  const repoPaths = githubUrls.map(url => {
-    const match = url.match(/github\.com\/([^\/]+\/[^\/]+)/);
-    return match ? match[1] : null;
-  }).filter(Boolean);
+  const repoPaths = githubUrls.map((url) => {
+    const match = url.match(/github\.com\/([^\/]+\/[^\/]+)/)
+    return match ? match[1] : null
+  }).filter(Boolean)
 
-  return `https://star-history.com/#${repoPaths.join('&')}`;
+  return `https://star-history.com/#${repoPaths.join('&')}`
 }
 
 async function askQuestion(query) {
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-  });
+    output: process.stdout,
+  })
 
-  return new Promise(resolve => {
-    rl.question(query, answer => {
-      rl.close();
-      resolve(answer);
-    });
-  });
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      rl.close()
+      resolve(answer)
+    })
+  })
 }
 
 async function processInput() {
   const inputRl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-  });
+    output: process.stdout,
+  })
 
-  console.log('Enter URLs (Ctrl+D to finish):');
+  console.log('Enter URLs (Ctrl+D to finish):')
 
-  const lines = [];
-  for await (const line of inputRl) {
-    lines.push(line);
-  }
-  inputRl.close();
+  const lines = []
+  for await (const line of inputRl)
+    lines.push(line)
+
+  inputRl.close()
 
   try {
-    const input = lines.join('\n');
-    const githubUrls = extractGitHubUrls(input);
-    const starHistoryLink = generateStarHistoryLink(githubUrls);
-    console.log('StarHistory Link:');
-    console.log(starHistoryLink);
+    const input = lines.join('\n')
+    const githubUrls = extractGitHubUrls(input)
+    const starHistoryLink = generateStarHistoryLink(githubUrls)
+    console.log('StarHistory Link:')
+    console.log(starHistoryLink)
 
-    const answer = await askQuestion('Do you want to open the link? (yes/no) ');
-    if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes') {
-      await open(starHistoryLink);
-    }
-  } catch (error) {
-    console.error(`An error occurred: ${error.message}`);
+    const answer = await askQuestion('Do you want to open the link? (yes/no) ')
+    if (answer.toLowerCase() === 'y' || answer.toLowerCase() === 'yes')
+      await open(starHistoryLink)
+  }
+  catch (error) {
+    console.error(`An error occurred: ${error.message}`)
   }
 }
 
-processInput();
+processInput()
